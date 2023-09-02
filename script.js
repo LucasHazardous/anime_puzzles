26d6658ecace542bf440bc1eb6549f5bbf4c82ab
LucasHazardous <70599201+LucasHazardous@users.noreply.github.com>
2023-09-02 21:16:43 +0200

    script refactorization

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ ctx.fillStyle = "black";
 let image = new Image();
 const new_width = 400;
 
-const pieces = new Map();
 const cols = 3;
 const rows = 5;
 
@@ -50,6 +49,72 @@ class Tile {
             ctx.drawImage(image, this.init_j*img_w, this.init_i*img_h, img_w, img_h, this.j*tile_w, this.i*tile_h, tile_w, tile_h);
         }
     }
+
+    static getTileByCoords(x, y) {
+        const i = Math.ceil(x/tile_w)-1;
+        const j = Math.ceil(y/tile_h)-1;
+        return String([
+            j == rows ? j-1 : j,
+            i == cols ? i-1 : i
+        ]);
+    }
+}
+
+class Tiles {
+    constructor() {
+        this.tiles = new Map();
+        this.hollow = null;
+        this.hollowKey = null;
+
+        for (let i = 0; i < rows; i++) {
+            for (let j = 0; j < cols; j++) {
+                const key = String([i,j]);
+                const newTile = new Tile(i, j);
+                this.tiles.set(key, newTile);
+                newTile.drawAtCurrentPos();
+            }
+        }
+
+        this.hollowKey = String([rows-1, cols-1]);
+        this.hollow = this.tiles.get(this.hollowKey);
+        this.hollow.is_hollow = true;
+        this.hollow.drawAtCurrentPos();
+    }
+
+    availableToSwitch() {
+        const res = [];
+        const toCheck = [[1, 0], [-1, 0], [0, 1], [0, -1]];
+
+        toCheck.forEach(fieldMod => {
+            const other_i = this.hollow.i + fieldMod[0];
+            const other_j = this.hollow.j + fieldMod[1];
+
+            if(other_i > -1 && other_j > -1 && other_i < rows && other_j < cols)
+            res.push(String([other_i, other_j]));
+        });
+
+        return res;
+    }
+
+    switchWithHollow(chosenTileKey) {
+        const chosenTile = this.tiles.get(chosenTileKey);
+        chosenTile.changePosTo(this.hollowKey);
+        this.hollow.changePosTo(chosenTileKey);
+
+        this.tiles.set(this.hollowKey, chosenTile);
+        this.tiles.set(chosenTileKey, this.hollow);
+
+        this.hollowKey = chosenTileKey;
+
+        this.hollow.drawAtCurrentPos();
+        chosenTile.drawAtCurrentPos();
+    }
+
+    randomize() {
+        for(let i = 0; i < 100; i++) {
+            this.switchWithHollow(chooseRandomElement(this.availableToSwitch()));
+        }
+    }
 }
 
 image.onload = () => {
@@ -65,70 +130,15 @@ image.onload = () => {
 
     ctx.fillRect(0, 0, new_width, new_height);
 
-    divide();
-};
+    const tiles = new Tiles();
 
-function divide() {
-    function detectTile(x, y) {
-        const i = Math.ceil(x/tile_w)-1;
-        const j = Math.ceil(y/tile_h)-1;
-        return String([
-            j == rows ? j-1 : j,
-            i == cols ? i-1 : i
-        ]);
-    }
-    
-    for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < cols; j++) {
-            const key = String([i,j]);
-            pieces.set(key, new Tile(i, j));
-            pieces.get(key).drawAtCurrentPos();
-        }
-    }
-
-    let hollowKey = String([rows-1, cols-1]);
-    const hollow = pieces.get(hollowKey);
-    hollow.is_hollow = true;
-    hollow.drawAtCurrentPos();
-
-    function availableToSwitch() {
-        const res = [];
-        const toCheck = [[1, 0], [-1, 0], [0, 1], [0, -1]];
-
-        toCheck.forEach(fieldMod => {
-            const other_i = hollow.i + fieldMod[0];
-            const other_j = hollow.j + fieldMod[1];
-
-            if(other_i > -1 && other_j > -1 && other_i < rows && other_j < cols)
-            res.push(String([other_i, other_j]));
-        });
-
-        return res;
-    }
-
-    function switchTiles(chosenTileKey) {
-        const chosenTile = pieces.get(chosenTileKey);
-        chosenTile.changePosTo(hollowKey);
-        hollow.changePosTo(chosenTileKey);
-
-        pieces.set(hollowKey, chosenTile);
-        pieces.set(chosenTileKey, hollow);
-
-        hollowKey = chosenTileKey;
-
-        hollow.drawAtCurrentPos();
-        chosenTile.drawAtCurrentPos();
-    }
-
-    for(let i = 0; i < 100; i++) {
-        switchTiles(chooseRandomElement(availableToSwitch()));
-    }
+    tiles.randomize();
 
     function mouseClickSwitch(e) {
-        const chosenTileKey = detectTile(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+        const chosenTileKey = Tile.getTileByCoords(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
 
-        if(availableToSwitch().includes(chosenTileKey)) {
-            switchTiles(chosenTileKey);
+        if(tiles.availableToSwitch().includes(chosenTileKey)) {
+            tiles.switchWithHollow(chosenTileKey);
         }
     }
 
@@ -138,4 +148,4 @@ function divide() {
         canvas.removeEventListener("click", mouseClickSwitch);
         loadImage();
     });
-}
+};
